7234fa8fc5485796ffc324a6ea363dfafca3de6d
Enzo Derviche <enzoderviche00@gmail.com>
2021-07-03 19:38:15 -0300

    primer dia db

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
+const { 
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
@@ -30,10 +30,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Country, Activity } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Country.belongsToMany(Activity, { through: 'Country_Activity'});
+Activity.belongsToMany(Country, { through: 'Country_Activity'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
---
+++ api/src/models/Activity.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('activity', {
+        id: {
+            type: DataTypes.SERIAL,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dificulty:{
+            type: DataTypes.INTEGER,
+            validate:{
+                max: 5,
+                min:1
+            },
+            allowNull:true
+        },
+        duration:{
+            type:DataTypes.INTEGER
+        },
+        temporada: {
+            type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera'),
+        }
+    });
+};
